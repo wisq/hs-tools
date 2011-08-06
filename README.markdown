6ec6fd9e0c7c9b5c34239f043657679691eba478
Adrian Irving-Beer <wisq@wisq.net>
2011-08-06 16:46:53 -0400

    Add mod-repack tool.

--- README.markdown
+++ README.markdown
@@ -10,3 +10,12 @@ schema-reorder
 Given two Rails schema files, file1 and file2, schema-reorder will output file1 with each table's columns sorted to match file2's columns for the equivalent table.
 
 This is useful when trying to resyncronise a development schema.rb with a dump of the production schema, since each may have had migrations applied at different times (and hence different column orders).
+
+mod-repack
+----------
+
+Given a list of archives (ZIP/RAR/7Zip), mod-repack will extract each one to a temporary directory.  If the result is a single directory, it will recompress the archive (with extension "-repack.7z"), omitting the top-level directory.
+
+This was designed to deal with mods for "The Elder Scrolls: Oblivion" and the mod installer tool "Wrye Bash", which doesn't recognise mod archives unless the data directories are at the top level of the archive.
+
+(Obviously this tool could fail faster if it listed the contents before extracting them to see if they match expectations, but given that it deals with three different archivers with different output formats, it's a lot of extra work to do so.)
---
+++ mod-repack/mod-repack.hs
@@ -0,0 +1,90 @@
+import Prelude hiding (catch)
+import Control.Exception
+
+import System.FilePath
+import System.Exit
+import System.Directory
+import System.Posix.Directory
+
+import System.Environment (getArgs)
+import System.IO.Temp (withTempDirectory)
+import System.Process hiding (runCommand)
+
+data ModFile = Zip FilePath | Rar FilePath | SevenZip FilePath
+
+parseFile :: FilePath -> ModFile
+parseFile file = parse' $ takeExtension file
+  where
+    parse' ".zip" = Zip file
+    parse' ".rar" = Rar file
+    parse' ".7z"  = SevenZip file
+    parse' ".001" = (addFile ".001" . parseFile . dropExtension) file
+    parse' ext    = error $ "Unknown file type '" ++ ext ++ "'."
+
+addFile :: String -> ModFile -> ModFile
+addFile ext (SevenZip file) = SevenZip $ file ++ ext
+addFile ext (Rar file) = Rar $ file ++ ext
+addFile ext (Zip file) = Zip $ file ++ ext
+
+modFilePath :: ModFile -> FilePath
+modFilePath (Zip fp) = fp
+modFilePath (Rar fp) = fp
+modFilePath (SevenZip fp) = fp
+
+main :: IO ()
+main = do
+  args <- getArgs
+  path <- getWorkingDirectory
+  let absolute x = joinPath [path, x]
+  let files = map absolute args
+  results <- mapM (catchString . repackInTemp path) files
+  putStrLn $ showResults $ zip files results
+
+catchString :: IO () -> IO String
+catchString io = (io >> return "") `catch` handler
+  where handler (SomeException e) = return $ show e
+
+showResults :: [(FilePath, String)] -> String
+showResults results
+  | all (null . snd) results = "\nEverything repacked okay."
+  | otherwise = unlines $ "" : "The following errors occurred:" : showAllErrors results
+  where
+    showAllErrors = map showError . filter (not . null . snd)
+    showError (file, err) = "  " ++ file ++ ":\n    " ++ err
+
+repackInTemp :: FilePath -> FilePath -> IO ()
+repackInTemp base file = withTempDirectory base "repack." repackIn
+  where repackIn path = changeWorkingDirectory path >> repack (parseFile file)
+
+repack :: ModFile -> IO ()
+repack file = do
+  runCommand $ unpackCommand file
+  getDirectoryContents "." >>= return . onlyPath >>= changeWorkingDirectory
+  print $ packCommand $ outputFile file
+  runCommand $ packCommand $ outputFile file
+
+runCommand :: (String, [String]) -> IO ()
+runCommand (cmd, args) = do
+  code <- rawSystem cmd args
+  case code of
+    ExitSuccess -> return ()
+    ExitFailure val -> error $ "Command '" ++ cmd ++ "' exited with status " ++ show val ++ "."
+
+onlyPath :: [FilePath] -> FilePath
+onlyPath = onlyPath' . filter notDots
+  where
+    notDots x = x /= "." && x /= ".."
+    onlyPath' [x] = x
+    onlyPath' xs = error $ "Archive contains " ++ show (length xs) ++ " paths, cannot repack."
+
+unpackCommand :: ModFile -> (String, [String])
+unpackCommand (Zip file) = ("unzip", [file])
+unpackCommand (Rar file) = ("unrar", ["x", file])
+unpackCommand (SevenZip file) = ("7zr", ["x", file])
+
+packCommand :: ModFile -> (String, [String])
+packCommand (SevenZip file) = ("7zr", ["a", file, "."])
+packCommand _ = error "not expected"
+
+outputFile :: ModFile -> ModFile
+outputFile = SevenZip . (++ "-repack.7z") . dropExtensions . modFilePath
